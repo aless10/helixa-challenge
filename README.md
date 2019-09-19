8176b5fab0473f80dca6bd08507888df9ac36b8d
aless10 <alessio.izzo86@gmail.com>
2019-09-19 21:06:27 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Helixa Challenge
+
+The project concerns the creation of an REST API for finding items within a tree. Starting from 2 JSON structured at several levels, the API must allow to search in each of them (separately) the last level elements that contain a given string in 2 modes:
+
+Search for the leaves on the whole tree
+Search for leaves on a specific sublayer (passed in input to the API)
+ 
+A plus is the realization of automatic tests.
+
+Attached are the 2 JSON files:
+* categories.json
+* psyhographics.json
+
+### Categories.json 
+The search must be done for the name field. The children are identified by the children field.
+### Psychographics.json
+The research must be done for the label field. The children are identified by the values ​​field.
+
+## How to run the application
+
+TODO
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# flake8: noqa
+
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+
+with open("requirements.txt", 'r') as inst_reqs:
+    install_requires = [str(req) for req in parse_requirements(inst_reqs)]
+
+packages = find_packages(include=['src', 'src.*'])
+
+setup(
+    name='helixa_api',
+    version='0.0.1.dev',
+    author='Alessio Izzo',
+    author_email='alessio.izzo86@gmail.com',
+    description='An Helixa challenge application',
+    long_description=__doc__,
+    packages=packages,
+    install_requires=install_requires,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Topic :: System :: Software Distribution',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
